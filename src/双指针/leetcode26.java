9804e8436ff610657eb1a43f5414cf82812f32e4
xuaise <1026413055@qq.com>
2020-02-22 20:41:15 +0800

    leetcode 61

---
+++ src/双指针/leetcode26.java
@@ -0,0 +1,14 @@
+package 双指针;
+
+public class leetcode26 {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int j = 1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
