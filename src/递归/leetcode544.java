1d4d1e75c88fa8dbcd71d774b2218154047e1c63
xuaise <1026413055@qq.com>
2020-02-22 12:07:40 +0800

    leetcode 544

---
+++ src/递归/leetcode544.java
@@ -0,0 +1,28 @@
+package 递归;
+
+public class leetcode544 {
+    int[] team;
+    int t;
+    StringBuilder ans;
+    public String findContestMatch(int n) {
+        team = new int[n];
+        t = 0;
+        ans = new StringBuilder();
+        write(n, Integer.numberOfTrailingZeros(n));
+        return ans.toString();
+    }
+
+    public void write(int n, int round) {
+        if (round == 0) {
+            int w = Integer.lowestOneBit(t);
+            team[t] = w > 0 ? n / w + 1 - team[t - w] : 1;
+            ans.append("" + team[t++]);
+        } else {
+            ans.append("(");
+            write(n, round - 1);
+            ans.append(",");
+            write(n, round - 1);
+            ans.append(")");
+        }
+    }
+}
